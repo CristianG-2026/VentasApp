7d0a184749aeeacdc95fddc1da882a6b2653bcc3
Cristian Jimenez <cristrianj2026@gmail.com>
2026-06-08 02:10:33 -0500

    Pruebas de login - Creador Cristian Jimenez

--- app/src/main/java/pe/idat/ventasapp/LoginActivity.kt
+++ app/src/main/java/pe/idat/ventasapp/LoginActivity.kt
@@ -1,20 +1,37 @@
 package pe.idat.ventasapp
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString().trim()
+            val password = etPassword.text.toString().trim()
+
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Por favor, llene todos los campos", Toast.LENGTH_SHORT).show()
+            } else {
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+
+                finish()
+            }
         }
     }
 }
