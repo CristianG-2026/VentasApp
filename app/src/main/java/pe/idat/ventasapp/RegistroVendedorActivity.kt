ab1c04ee61f733a64c361810f9e363824e8f21e3
Jose Urquia <djurqd@gmail.com>
2026-06-26 22:49:51 -0500

    RegistroVendedorActivity modificaciones - Creador Jose Urquia

---
+++ app/src/main/java/pe/idat/ventasapp/RegistroVendedorActivity.kt
@@ -0,0 +1,12 @@
+package pe.idat.ventasapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RegistroVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_vendedor)
+
+    }
+}
